918c7dea39e23bb1936bb1251de9fdce7d3eefa4
Chetanmitra <czm5805@psu.edu>
2020-09-01 21:07:35 +0000

    Grammar

--- main.c
+++ main.c
@@ -9,9 +9,9 @@
 
 
 int main(void) {
-  char *Celsius = readline("Enter temperatures in Celsius: ");
+  char *Celsius = readline("Enter temperature in celsius: ");
   double Cel = atof(Celsius);
   double far = 1.8*Cel+32;
-  printf("%f° in Celsius is equivalent to %f Fahrenheit°\n",Cel, far);
+  printf("%f° in Celsius is equivalent to %f Fahrenheit°.\n",Cel, far);
   return 0;
 }
