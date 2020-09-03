c4b7c6c55a9a4d82cb3858803d62e68d9b364192
Chetanmitra <czm5805@psu.edu>
2020-09-03 23:58:17 +0000

    space

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
+// Author: Chetan Mitra czm5805@psu.edu
+// Collaborator: Tyler Ciuca tmc6093@psu.edu
+// Collaborator: Dymea Schippers dxs5940@psu.edu
+// Collaborator: Jian Hong Weng jpw6087@psu.edu
+
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
-
-//Author: Chetan Mitra czm5805@psu.edu
-//Collaborator: Tyler Ciuca tmc6093@psu.edu
-//Collaborator: Dymea Schippers dxs5940@psu.edu
-//Collaborator: Jian Hong Weng jpw6087@psu.edu
 
 
 int main(void) {
